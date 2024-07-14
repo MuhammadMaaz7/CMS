ba154ec6ec44e1a702942bb8568e9c6e0216a5f3
MuhammadMaaz7 <maazawan100@gmail.com>
2024-07-14 13:39:45 +0500

    updates

--- cms/cms/doctype/complaint_submitting/complaint_submitting.js
+++ cms/cms/doctype/complaint_submitting/complaint_submitting.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Maaz and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Complaint Submitting To', {
+frappe.ui.form.on('Complaint Submitting', {
 	// refresh: function(frm) {
 
 	// }
---
+++ cms/cms/doctype/complaint_submitting/test_complaint_submitting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Maaz and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestComplaintSubmitting(FrappeTestCase):
+	pass

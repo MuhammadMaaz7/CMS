f9dd1f5ec30ca3019b37c3318d9145c664df4243
MuhammadMaaz7 <maazawan100@gmail.com>
2024-07-17 10:44:24 +0500

    complaint report

---
+++ cms/cms/report/complaints_report/complaints_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Maaz and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Complaints Report"] = {
+	"filters": [
+
+	]
+};
